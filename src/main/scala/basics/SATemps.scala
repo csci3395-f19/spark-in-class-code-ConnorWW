555613a504a3f74df978e4b924a3b62637512bd1
Connor <cweisenb@trinity.edu>
2019-08-27 17:05:09 -0500

    Pushing new file SATemps.scala

---
+++ src/main/scala/basics/SATemps.scala
@@ -0,0 +1,50 @@
+package basics
+
+case class TempRow(
+    day: Int,
+    doy: Int,
+    month: Int,
+    year: Int,
+    precip: Double,
+    tave: Double,
+    tmax: Double,
+    tmin: Double
+)
+
+object SATemps {
+
+  def parseLine(line: String): TempRow = {
+    val p = line.split(",")
+    TempRow(
+      p(0).toInt,
+      p(1).toInt,
+      p(2).toInt,
+      p(4).toInt,
+      p(5).toDouble,
+      p(6).toDouble,
+      p(7).toDouble,
+      p(8).toDouble
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+	val source = scala.io.Source.fromFile("/users/mlewis/CSCI3395-F19/InClassBD/data/SanAntonioTemps.csv")
+	
+	val lines = source.getLines()
+	val data = lines.drop(2).map(parseLine).toArray
+  val highestTempRow = data.maxBy(_.tmax)
+  println("Date of highest temperature: " + highestTempRow.month + "/" + highestTempRow.day + "/" + highestTempRow.year)
+  val highestRainRow = data.maxBy(_.precip)
+  println("Date of most precipitation: " + highestRainRow.month + "/" + highestRainRow.day + "/" + highestRainRow.year)
+
+  val rowsWithRainOverOneInch = data.filter(_.precip >= 1.0)
+  println("Fraction of days with more than one inch of rain: " + rowsWithRainOverOneInch.length + " / " + data.length)
+
+  val rainyTemp = rowsWithRainOverOneInch.fold(0)((accumulated:Double, row:TempRow) => accumulated + row2.tmax)(0). / rowsWithRainOverOneInch.length
+
+
+	source.close()
+  }
+
+  
+}
